d393938ae48da18a1b281d5eeb116f44ece59eaf
Hussein Hany <husseinhany@192.168.1.20>
2023-11-29 01:11:58 +0200

    Create endpoints for Books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const Book = require('../models/book.js');
 const User = require('../models/user.js');
 
-// get all books async
-module.exports.getAllBooks = async (req, res) => {
+// get all books
+exports.getAllBooks = async (req, res) => {
     try {
         const books = await Book.find({});
         res.status(200).json(books);
@@ -14,5 +14,70 @@ module.exports.getAllBooks = async (req, res) => {
         res.status(500).json(err);
     }
 }
+
+// get book by id
+exports.getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        res.status(200).json(book);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// get book by title
+exports.getBookByTitle = async (req, res) => {
+    try {
+        const book = await Book.findOne({title: req.params.title});
+        res.status(200).json(book);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// get book by author
+exports.getBookByAuthor = async (req, res) => {
+    try {
+        const book = await Book.findOne({author: req.params.author});
+        res.status(200).json(book);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// get book by publisher
+exports.getBookByPublisher = async (req, res) => {
+    res.send('get book by publisher');
+}
+
+// get book by category
+exports.getBookByCategory = async (req, res) => {
+    res.send('get book by category');
+}
+
+// create book
+exports.createBook_get = async (req, res) => {
+    res.send('create book get');
+}
+exports.createBook_post = async (req, res) => {
+    res.send('create book post');
+}
+
+// update book by id
+exports.updateBook_get = async (req, res) => {
+    res.send('update book get');
+}
+exports.updateBook_post = async (req, res) => {
+    res.send('update book post');
+}
+
+// delete book by id
+exports.deleteBook_get = async (req, res) => {
+    res.send('delete book get');
+}
+exports.deleteBook_post = async (req, res) => {
+    res.send('delete book post');
+}
+
 
 
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,36 @@
 // use bookController.js instead:
-
-// routes for books
-// =============================================================
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController.js');
 
 // get all books
 router.get('/', bookController.getAllBooks);
+// get book by title
+router.get('/title/:title', bookController.getBookByTitle);
+
+// get book by author
+router.get('/author/:author', bookController.getBookByAuthor);
+
+// get book by publisher
+router.get('/publisher/:publisher', bookController.getBookByPublisher);
+
+// get book by category
+router.get('/category/:category', bookController.getBookByCategory);
+
+// create book
+router.get('/create', bookController.createBook_get);
+router.post('/create', bookController.createBook_post);
+
+// update book by id
+router.get('/:id/update', bookController.updateBook_get);
+router.post('/:id/update', bookController.updateBook_post);
+
+// delete book by id
+router.get('/:id/delete', bookController.deleteBook_get);
+router.post('/:id/delete', bookController.deleteBook_post);
+
+// get book by id
+router.get('/:id', bookController.getBookById);
 
 // export
 module.exports = router;
