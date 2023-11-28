28123f5e1f067b5280fb1598dc53c61fdd5fab6c
Hussein Hany <husseinhany@192.168.1.20>
2023-11-29 00:24:39 +0200

    Fix typo in first name

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    fisrt_name: {
+    first_name: {
         type: String,
         required: true,
     },
@@ -35,7 +35,7 @@ const userSchema = new Schema({
 });
 
 userSchema.virtual('fullName').get(function () {
-    return `${this.fisrt_name} ${this.last_name}`;
+    return `${this.first_name} ${this.last_name}`;
 });
 
 module.exports = mongoose.model('User', userSchema);
