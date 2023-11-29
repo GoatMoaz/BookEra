555e882fb141062508988761e33348027b9877bb
HusseinSaad <hussein.saad2021@gmail.com>
2023-11-29 14:33:02 +0200

    Update order routes

--- routes/orders.js
+++ routes/orders.js
@@ -5,8 +5,6 @@ const orderController = require('../controllers/orderController.js');
 // get all orders
 router.get('/', orderController.order_list);
 
-// get one order
-router.get('/:id', orderController.order_detail);
 
 // create one order
 router.get('/create', orderController.order_create_get);
@@ -23,5 +21,8 @@ router.post('/:id/update', orderController.order_update_post);
 // delete one order
 router.get('/:id/delete', orderController.order_delete_get);
 router.post('/:id/delete', orderController.order_delete_post);
+
+// get one order
+router.get('/:id', orderController.order_detail);
 
 module.exports = router;
