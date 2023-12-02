b0043fb6d64b116c557a60320a9fa975ff591f12
Hussein Hany <husseinhany@192.168.1.20>
2023-12-02 22:59:06 +0200

    Rendered 'books.ejs' with the title 'Books' and passed the 'books' array to display detailed book information

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,7 +4,8 @@ const Book = require('../models/book.js');
 exports.getAllBooks = async (req, res) => {
     try {
         const books = await Book.find({});
-        res.status(200).json(books);
+        // send books to books.ejs
+        res.render('books', { title: 'Books', books: books });
     } catch (err) {
         res.status(500).json(err);
     }
