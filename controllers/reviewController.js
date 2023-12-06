538a2170ec974595f028328056f27b0f68703b01
Hussein Hany <husseinhany@192.168.1.20>
2023-12-06 22:45:32 +0200

    Implement delete review functionality

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -31,5 +31,14 @@ exports.deleteReview_get = async (req, res) => {
     res.send('NOT IMPLEMENTED: deleteReview GET');
 }
 exports.deleteReview_post = async (req, res) => {
-    res.send('NOT IMPLEMENTED: deleteReview POST');
+    // must be deleted by the user who created it
+    const review = await Review.findById(req.params.id);
+    if (review.user.toString() !== req.user._id.toString()) {
+        req.flash('error', 'You are not authorized to do that!');
+        return res.status(401).send('Unauthorized');
+    }
+    await Review.findByIdAndDelete(req.params.id);
+    req.flash('success', 'Review deleted successfully!');
+    // refresh the page
+    res.redirect('back');
 }
