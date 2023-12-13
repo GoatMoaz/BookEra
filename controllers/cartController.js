cc504c78afd3dce0e7c9e64b69dc09fe43ee3cb2
husseinhany <hh1681@fayoum.edu.eg>
2023-12-13 22:23:02 +0200

    Implement delete item from cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -15,5 +15,24 @@ exports.addToCart = async function(req, res, next) {
     cart.books.push(bookId);
     await cart.save();
 
-    res.redirect('/books');
+    res.redirect('back');
+};
+
+
+// delete book from cart
+exports.deleteFromCart = async function(req, res, next) {
+    const userId = req.user._id;
+    const bookId = req.params.id;
+
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) {
+        cart = new Cart({ user: userId });
+    }
+
+
+    cart.books.pull(bookId);
+    await cart.save();
+
+    // refresh the page
+    res.redirect('back');
 };
