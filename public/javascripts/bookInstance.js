0a062f996f3c546b74a06e3984e885650ab9656e
husseinhany <hh1681@fayoum.edu.eg>
2023-12-16 18:42:05 +0200

    Merge instance design and pdf logic in one file

--- public/javascripts/bookInstance.js
+++ public/javascripts/bookInstance.js
@@ -1,7 +1,8 @@
 window.onload = function () {
-    const canvas = document.getElementById('pdf-canvas');
-    const url = canvas.getAttribute('data-pdf-url');
-
+    // PDF.js code
+    const pdfCanvas = document.getElementById('pdf-canvas');
+    const url = pdfCanvas.getAttribute('data-pdf-url');
+    console.log(url)
     pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
 
     const loadingTask = pdfjsLib.getDocument(url);
@@ -15,9 +16,9 @@ window.onload = function () {
             const scale = 1.5;
             const viewport = page.getViewport({scale: scale});
 
-            const context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
+            const context = pdfCanvas.getContext('2d');
+            pdfCanvas.height = viewport.height;
+            pdfCanvas.width = viewport.width;
 
             const renderContext = {
                 canvasContext: context,
@@ -31,6 +32,64 @@ window.onload = function () {
     }, function (reason) {
         console.error(reason);
     });
+
+    // Image resizing code
+    const img = document.getElementById('myImage');
+    const resizerCanvas = document.getElementById('resizer');
+    const resizerCtx = resizerCanvas.getContext('2d');
+    const newWidth = 350;
+    img.onload = function () {
+        const newHeight = (newWidth / img.width) * img.height;
+        resizerCanvas.width = newWidth;
+        resizerCanvas.height = newHeight;
+
+        resizerCtx.imageSmoothingEnabled = true;
+
+        resizerCtx.drawImage(img, 0, 0, newWidth, newHeight);
+    };
+    img.src = document.querySelector('#myImage').src;
+
+    // Rating code
+    let currentRating = 0;
+    let ratingLocked = false;
+
+    function hoverStars(stars) {
+        if (!ratingLocked) {
+            const allStars = document.querySelectorAll('.stars');
+            allStars.forEach((star, index) => {
+                star.classList.remove('rated');
+                if (index < stars) {
+                    star.classList.add('rated');
+                }
+            });
+        }
+    }
+
+    function rate(stars) {
+        if (!ratingLocked) {
+            currentRating = stars;
+            document.getElementById('rating').value = stars;
+            ratingLocked = true;
+            displayRating();
+        } else {
+            currentRating = 0;
+            document.getElementById('rating').value = stars;
+            ratingLocked = false;
+            displayRating();
+        }
+    }
+
+    function displayRating() {
+        const ratingElement = document.getElementById('rating');
+        ratingElement.innerHTML = `You've rated this ${currentRating} star(s).`;
+        const stars = document.querySelectorAll('.stars');
+        stars.forEach((star, index) => {
+            star.classList.remove('rated');
+            if (index < currentRating) {
+                star.classList.add('rated');
+            }
+        });
+    }
 }
 const button = document.querySelector(".cart");
 const done = document.querySelector(".done");
@@ -47,3 +106,46 @@ button.addEventListener('click',()=>{
     }
 
 });
+;
+let currentRating = 0;
+let ratingLocked = false;
+
+function hoverStars(stars) {
+
+    if (!ratingLocked) {
+        const allStars = document.querySelectorAll('.stars');
+        allStars.forEach((star, index) => {
+            star.classList.remove('rated');
+            if (index < stars) {
+                star.classList.add('rated');
+            }
+        });
+    }
+}
+
+function rate(stars) {
+    if (!ratingLocked) {
+        currentRating = stars;
+        document.getElementById('rating').value = stars;
+        ratingLocked = true;
+        displayRating();
+    } else {
+
+        currentRating = 0;
+        document.getElementById('rating').value = stars;
+        ratingLocked = false;
+        displayRating();
+    }
+}
+
+function displayRating() {
+    const ratingElement = document.getElementById('rating');
+    ratingElement.innerHTML = `You've rated this ${currentRating} star(s).`;
+    const stars = document.querySelectorAll('.stars');
+    stars.forEach((star, index) => {
+        star.classList.remove('rated');
+        if (index < currentRating) {
+            star.classList.add('rated');
+        }
+    });
+}
