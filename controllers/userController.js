b2727b0513533e94f6fc7e121c04aa9cad7b851b
HusseinSaad <hussein.saad2021@gmail.com>
2023-12-17 16:08:28 +0200

    Add gender field to user signup form

--- controllers/userController.js
+++ controllers/userController.js
@@ -88,6 +88,7 @@ exports.signup_post = async (req, res) => {
             username: req.body.username,
             password: bcrypt.hashSync(req.body.password, 10),
             wallet_amount: 0,
+            gender: req.body.gender,
             type: req.body.type,
             address: req.body.address,
         });
